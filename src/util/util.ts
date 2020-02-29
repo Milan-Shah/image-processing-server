7df37d712bf2083c2710fb71b98aa4852c610a92
Milan Shah <mshah1992@live.com>
2020-02-29 14:00:00 -0500

    Added Postman collection for testing

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import Jimp = require('jimp');
+import { reject } from 'bluebird';
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,8 +10,15 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
+    return new Promise( async (resolve, reject) => {
+
+        let photo;
+        try {
+            photo = await Jimp.read(inputURL);
+        } catch (error) {
+            return reject('Can not read the image within provided inputURL')
+        }
+        
         const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
         await photo
         .resize(256, 256) // resize
